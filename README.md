8aca300111ee65df4a87d33e16a3b13ab7933c44
Jonas Jilmefors <jonas@jilmefors.se>
2023-01-05 11:33:11 +0100

    Initial commit
    - Including a Angular shell service

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# micro-frontend
+# Frontend MicroServices Demo
+This repo contains a Angular shell frontend microservices example.
---
+++ shell/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'page2',
+    loadChildren: () => import('./page2/page2.module').then(m => m.Page2Module),
+    data: { title: 'FE - MicroServices page 2' }
+  },
+  {
+    path: 'page3',
+    loadChildren: () => import('./page3/page3.module').then(m => m.Page3Module),
+    data: { title: 'FE - MicroServices page 3' }
+  },
+  {
+    path: '',
+    loadChildren: () => import('./page1/page1.module').then(m => m.Page1Module),
+    data: { title: 'FE - MicroServices Start' }
+  },
+  {
+    path: '**',
+    loadChildren: () => import('./page1/page1.module').then(m => m.Page1Module),
+    data: { title: 'FE - MicroServices Start' }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
